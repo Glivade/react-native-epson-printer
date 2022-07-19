bd68ae43b3c7d08af314a540cbd66b2b3b2aa246
alex-pollard <64681690+alex-pollard@users.noreply.github.com>
2022-07-19 14:34:40 -0700

    Update EpsonPrinterModule.kt

--- android/src/main/java/com/reactnativeepsonprinter/EpsonPrinterModule.kt
+++ android/src/main/java/com/reactnativeepsonprinter/EpsonPrinterModule.kt
@@ -154,8 +154,8 @@ class EpsonPrinterModule(reactContext: ReactApplicationContext) :
                     val socket = Socket(target, 9100)
                     socket.getOutputStream().write(fontSize)
                     for (i in 0 until receiptCopyCount) {
-                        socket.getOutputStream().write(data.toByteArray())
-                        socket.getOutputStream().write("\n".toByteArray())
+                        socket.getOutputStream().write(data.toByteArray(Charsets.UTF_8))
+                        socket.getOutputStream().write("\n".toByteArray(Charsets.UTF_8))
                         socket.getOutputStream().write(PrintUtil.CUT_PAPER)
                     }
                     socket.getOutputStream().close()
